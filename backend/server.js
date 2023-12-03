5ed62992e3122cd062bb39f5ecf21ae2ef75acc7
Robert Duebelbeis <6600820+duebelbytes@users.noreply.github.com>
2023-12-02 22:38:26 -0700

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/leon", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("Connected successfully to MongoDB");
+});
